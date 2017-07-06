3e9c6c7eea7967f38efaccf5d08ddefa2aeabfab
Malekbenz <malekbenz@gmail.com.com>
2017-07-06 15:55:21 +0100

    add resultStats

--- js/app.js
+++ js/app.js
@@ -1,94 +1,99 @@
-     var $query = null;
-        var data = {
-            "query": {
+var $query = null;
+var data = {
+    "query": {
+        "bool": {
+            "must": {
                 "bool": {
-                    "must": {
-                        "bool": {
-                            "should": [
-                                { "match": { "nom": "mohamed" } },
-                                { "match": { "prenom": "ahmed" } }]
-                        }
-                    }
+                    "should": [
+                        { "match": { "nom": "mohamed" } },
+                        { "match": { "prenom": "ahmed" } }]
                 }
             }
         }
+    }
+}
 
 
-        function query(term) {
-            var data = {
-                "query": {
-                    "term": { "nom": term }
-                }
-            };
-
-            data = {
-                "query": {
-                    "multi_match": {
-                        "query": term,
-                        "fields": ["nom", "prenom"]
-                    }
-                }
-            };
-            data = {
-                "query": {
-                    "query_string": {
-                        
-                        "query": term
-                    }
-                }
-            };
-            console.log(data);
-            return JSON.stringify(data);
-
+function query(term) {
+    var data = {
+        "query": {
+            "term": { "nom": term }
         }
+    };
 
-        function showResult(data) {
-            console.log(data.hits)
-            $("#result").empty();
-
-            data.hits.hits.map(function (item) {
-                return item._source.id + " " + item._source.nom + " " + item._source.prenom + " " + item._source.dateNaissance;
-            })
-                .forEach(function (item) {
-                    $("#result")
-                        .append("<li>" + item + "</li>");
-
-                });
+    data = {
+        "query": {
+            "multi_match": {
+                "query": term,
+                "fields": ["nom", "prenom"]
+            }
         }
+    };
+    data = {
+        "query": {
+            "query_string": {
 
-        function search(e) {
-            console.log("Clicked");
-            e.preventDefault();
-            $.ajax({
-                method: "POST",
-                // async:false,
-                // crossDomain: true, 
-                // dataType : 'json',
-                data: query($query.val()),
-                contentType: 'application/json',
-                // beforeSend: function (xhr) {
-                //     xhr.setRequestHeader('Authorization', 'Basic ' + btoa('elastic:changeme'));
-                // },
-                url: "/api/_search",
-                success: showResult,
-                error: function (err) { console.log(err); }
-
-
-            });
-
-
+                "query": term
+            }
         }
+    };
+    // console.log(data);
+    return JSON.stringify(data);
 
-        $(function () {
-            $query = $("#query");
-            $("#btnSimpleSearch").click(search);
-            $("#query").on('input', search);
+}
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+function showResult(data) {
+    console.log(data.hits)
+    $("#result").empty();
+    // $('input.number').number( true, 2 );
+    $("#resultStats").text("About " + numberWithCommas(data.hits.total) + " results ( " + data.took / 1000 + " seconds) ");
 
-            $("#query").on('input', function () {
-                console.log('change');
 
-            });
+    data.hits.hits.map(function (item) {
+        return item._source.id + " " + item._source.nom + " " + item._source.prenom + " " + item._source.dateNaissance;
+    })
+        .forEach(function (item) {
+            $("#result")
+                .append("<li>" + item + "</li>");
+
+        });
+}
+
+function search(e) {
+
+    e.preventDefault();
+    $.ajax({
+        method: "POST",
+        // async:false,
+        // crossDomain: true, 
+        // dataType : 'json',
+        data: query($query.val()),
+        contentType: 'application/json',
+        // beforeSend: function (xhr) {
+        //     xhr.setRequestHeader('Authorization', 'Basic ' + btoa('elastic:changeme'));
+        // },
+        url: "/api/_search",
+        success: showResult,
+        error: function (err) { console.log(err); }
+
+
+    });
+
+
+}
+
+$(function () {
+    $query = $("#query");
+    $("#btnSimpleSearch").click(search);
+    $("#query").on('input', search);
+
+    $("#query").on('input', function () {
+        console.log('change');
+
+    });
 
 
 
-        })
+})
