16e3acd56990e69148fe30f29b61b6d6e3b465eb
Malekbenz <malekbenz@gmail.com.com>
2017-07-15 17:31:50 +0100

    add localstorage

---
+++ src/app/services/localstorage.service.js
@@ -0,0 +1,36 @@
+// storage.js
+(function (app) {
+    'use strict';
+
+
+    app.factory('storage', storage)
+
+    storage.$inject = ['$localStorage', '$q'];
+
+    function storage($localStorage, $q) {
+        return {
+            getData: getData,
+            saveData: saveData
+        };
+
+        var storageName = "redd";
+        
+        function getData() {
+            var deferred = $q.defer();
+            var data = $localStorage[storageName];
+            if(!data){
+                return null ;
+            } 
+
+            deferred.resolve(data);
+            return deferred.promise;
+
+        }
+
+        function saveData(data) {
+            $localStorage[storageName] = data;
+        }
+
+
+    }
+})(angular.module('app'));
